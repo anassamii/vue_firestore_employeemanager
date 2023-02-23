bcec19999df7bef7f662f8ac26eb7b5e2769c5d5
Anas SAMII <samiianas000@gmail.com>
2023-02-23 14:18:24 +0100

    changes due to setup

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -4,5 +4,5 @@ export default {
   projectId: "vuejs-123de",
   storageBucket: "vuejs-123de.appspot.com",
   messagingSenderId: "846452003657",
-  appId: "1:846452003657:web:60e4aeb6e73ab8b9ab8098"
+  appId: "81:846452003657:web:60e4aeb6e73ab8b9ab8098"
 };
